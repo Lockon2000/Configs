232f812165e28a96d440619994519718f191139b
Mohamed Abdelwahab <m.taha2000@outlook.de>
2018-12-26 23:48:45 +0100

    Modifications to: PowerShell Profile, Vagrantfiles for VMs, ZOC HostDirectory.zocini

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,11 @@
 Import-Module LockLib 3>$null
 
 
-New-Alias -Name sublime -Value "C:\Program Files\Sublime Text 3\sublime_text.exe"
-New-Alias -Name m       -Value more
-New-Alias -Name less    -Value more
-New-Alias -Name timeit  -Value Measure-Command
+New-Alias -Name sublime     -Value "C:\Program Files\Sublime Text 3\sublime_text.exe"
+New-Alias -Name m           -Value more
+New-Alias -Name less        -Value more
+New-Alias -Name timeit      -Value Measure-Command
+New-Alias -Name fullhistory -Value (Get-PSReadLineOption).HistorySavePath
 
 
 function Prompt {
