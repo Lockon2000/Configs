7f384ee740ddd930ad207442bca09bbfa56be3d9
Mohamed Abdelwahab <m.taha2000@outlook.de>
2020-08-10 23:21:16 +0200

    Update the VS Code extensions list

--- VS Code/execute/Install VS Code Extensions.ps1
+++ VS Code/execute/Install VS Code Extensions.ps1
@@ -1,27 +1,28 @@
 code --install-extension alexiv.vscode-angular2-files # Angular Files
 code --install-extension angular.ng-template # Angular Language Service
-code --install-extension bmewburn.vscode-intelephense-client # PHP Intelephense
+code --install-extension johnpapa.angular2 # Angular Snippets
+code --install-extension mrmlnc.vscode-apache # Apache conf
+code --install-extension vmsynkov.colonize # Colonize
+code --install-extension msjsdiag.debugger-for-chrome # Debugger for Chrome
+code --install-extension firefox-devtools.vscode-firefox-debug # Debugger for Firefox
+code --install-extension msjsdiag.debugger-for-edge # Debugger for Microsoft Edge
+code --install-extension mikestead.dotenv # DotENV
 code --install-extension dbaeumer.vscode-eslint # ESLint
 code --install-extension eamodio.gitlens # GitLens
-code --install-extension firefox-devtools.vscode-firefox-debug # Debugger for Firefox
-code --install-extension imperez.smarty # Smarty
-code --install-extension johnpapa.angular2 # Angular Snippets
-code --install-extension liximomo.sftp # SFTP
-code --install-extension mikestead.dotenv # DotENV
-code --install-extension mrmlnc.vscode-apache # Apache conf
-code --install-extension mrmlnc.vscode-scss # SCSS IntelliSense
+code --install-extension tyriar.lorem-ipsum # Lorem ipsum
+code --install-extension quicktype.quicktype # Paste JSON as Code
+code --install-extension felixfbecker.php-debug # PHP Debug
+code --install-extension bmewburn.vscode-intelephense-client # PHP Intelephense
+code --install-extension ms-vscode.powershell # PowerShell
 code --install-extension ms-python.python # Python
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack # Remote Development
-code --install-extension ms-vscode.powershell # PowerShell
-code --install-extension ms-vscode.vscode-typescript-tslint-plugin # TSLint
-code --install-extension msjsdiag.debugger-for-chrome # Debugger for Chrome
-code --install-extension msjsdiag.debugger-for-edge # Debugger for Microsoft Edge
-code --install-extension quicktype.quicktype # Paste JSON as Code
-code --install-extension redhat.vscode-yaml # YAML
 code --install-extension sibiraj-s.vscode-scss-formatter # SCSS Formatter
+code --install-extension mrmlnc.vscode-scss # SCSS IntelliSense
+code --install-extension liximomo.sftp # SFTP
+code --install-extension imperez.smarty # Smarty
 code --install-extension stylelint.vscode-stylelint # stylelint
-code --install-extension tyriar.lorem-ipsum # Lorem ipsum
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin # TSLint
 code --install-extension whatwedo.twig # Twig
+code --install-extension redhat.vscode-yaml # YAML
 
 Pause
-
